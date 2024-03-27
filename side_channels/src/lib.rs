ae148fea65d12562bdedfd71b40fb8e15ffa3d31
pinkforest <36498018+pinkforest@users.noreply.github.com>
2024-03-28 00:04:01 +1100

    think im done

--- side_channels/src/lib.rs
+++ side_channels/src/lib.rs
@@ -1,15 +1,20 @@
+#[cfg(arr_size = "100")]
+const ARR_SIZE: usize = 100;
+
+#[cfg(not(arr_size = "100"))]
+const ARR_SIZE: usize = 16;
 
 /// No this is not really Secret .. it's (super) Secret .. lol :P
 pub struct Secret128bit {
     /// We will leak this
-    pub secret: [u8; 16],
-}        
+    pub secret: [u8; ARR_SIZE],
+}
 
 impl Secret128bit {
     /// non-ct short-circuit, leaking informaton about secret
-    pub fn check_eq_vartime(&self, input: &[u8; 16]) -> bool {
-        for i in 0..=15 {
-            if input[i] != self.secret[i] {
+    pub fn check_eq_vartime(&self, input: &[u8; ARR_SIZE]) -> bool {
+        for i in 1..=ARR_SIZE {
+            if input[i - 1] != self.secret[i - 1] {
                 return false;
             }
         }
@@ -17,29 +22,63 @@ impl Secret128bit {
     }
 }
 
-#[cfg(test)]
-mod test {
+#[cfg(all(not(arr_size = "100"), test))]
+mod test_eq16 {
 
     #[test]
-    fn eq_vartime_yes() {
+    fn eq16_vartime_yes() {
         let against = b"1234567890abcdef";
-        let checker = super::Secret128bit { secret: *b"1234567890abcdef" };
+        let checker = super::Secret128bit {
+            secret: *b"1234567890abcdef",
+        };
         assert!(checker.check_eq_vartime(&against));
     }
 
-    // Branching introspection w/ below from MIR
-    
     #[test]
-    fn eq_vartime_middle() {
+    fn eq16_vartime_middle() {
         let against = b"12345678ZZZZZZZZ";
-        let checker = super::Secret128bit { secret: *b"1234567890abcdef" };
+        let checker = super::Secret128bit {
+            secret: *b"1234567890abcdef",
+        };
         assert_eq!(checker.check_eq_vartime(&against), false);
     }
-    
+
     #[test]
-    fn eq_vartime_start() {
+    fn eq16_vartime_start() {
         let against = b"ZZZZZZZZZZZZZZZZ";
-        let checker = super::Secret128bit { secret: *b"1234567890abcdef" };
+        let checker = super::Secret128bit {
+            secret: *b"1234567890abcdef",
+        };
         assert_eq!(checker.check_eq_vartime(&against), false);
-    }        
+    }
+}
+
+#[cfg(all(arr_size = "100", test))]
+mod test_eq100 {
+
+    use crate::ARR_SIZE;
+
+    #[test]
+    fn eq100_vartime_yes() {
+        let against: [u8; ARR_SIZE] = [0; ARR_SIZE];
+        let secret: [u8; ARR_SIZE] = [0; ARR_SIZE];
+        let checker = super::Secret128bit { secret: secret };
+        assert!(checker.check_eq_vartime(&against));
+    }
+
+    #[test]
+    fn eq100_vartime_middle() {
+        let against = [0; ARR_SIZE];
+        let secret: [u8; ARR_SIZE] = [[0; 50], [1; 50]].concat().try_into().unwrap();
+        let checker = super::Secret128bit { secret: secret };
+        assert_eq!(checker.check_eq_vartime(&against), false);
+    }
+
+    #[test]
+    fn eq100_vartime_start() {
+        let against = [0; ARR_SIZE];
+        let secret: [u8; ARR_SIZE] = [90; ARR_SIZE];
+        let checker = super::Secret128bit { secret: secret };
+        assert_eq!(checker.check_eq_vartime(&against), false);
+    }
 }
---
+++ side_channels_dudect/examples/ctbench-foo.rs
@@ -0,0 +1,71 @@
+use dudect_bencher::rand::Rng;
+use dudect_bencher::{ctbench_main_with_seeds, BenchRng, Class, CtRunner};
+
+// Return a random vector of length len
+fn rand_vec(len: usize, rng: &mut BenchRng) -> Vec<u8> {
+    let mut arr = vec![0u8; len];
+    rng.fill(arr.as_mut_slice());
+    arr
+}
+
+// Benchmark for some random arithmetic operations. This should produce small t-values
+fn arith(runner: &mut CtRunner, rng: &mut BenchRng) {
+    let mut inputs = Vec::new();
+    let mut classes = Vec::new();
+
+    // Make 100,000 inputs on each run
+    for _ in 0..100_000 {
+        inputs.push(rng.gen::<usize>());
+        // Randomly pick which distribution this example belongs to
+        if rng.gen::<bool>() {
+            classes.push(Class::Left);
+        } else {
+            classes.push(Class::Right);
+        }
+    }
+
+    for (u, class) in inputs.into_iter().zip(classes.into_iter()) {
+        // Time some random arithmetic operations
+        runner.run_one(class, || ((u + 10) / 6) << 5);
+    }
+}
+
+// Benchmark for equality of vectors. This does an early return when it finds an inequality, so it
+// should be very much not constant-time
+fn vec_eq(runner: &mut CtRunner, rng: &mut BenchRng) {
+    // Make vectors of size 100
+    let vlen = 100;
+    let mut inputs: Vec<(Vec<u8>, Vec<u8>)> = Vec::new();
+    let mut classes = Vec::new();
+
+    // Make 100,000 random pairs of vectors
+    for _ in 0..100_000 {
+        // Flip a coin. If true, make a pair of vectors that are equal to each other and put it
+        // in the Left distribution
+        if rng.gen::<bool>() {
+            let v1 = rand_vec(vlen, rng);
+            let v2 = v1.clone();
+            inputs.push((v1, v2));
+            classes.push(Class::Left);
+        }
+        // Otherwise, make a pair of vectors that differ at the 6th element and put it in the
+        // right distribution
+        else {
+            let v1 = rand_vec(vlen, rng);
+            let mut v2 = v1.clone();
+            v2[5] = 7;
+            inputs.push((v1, v2));
+            classes.push(Class::Right);
+        }
+    }
+
+    for (class, (u, v)) in classes.into_iter().zip(inputs.into_iter()) {
+        // Now time how long it takes to do a vector comparison
+        runner.run_one(class, || u == v);
+    }
+}
+
+// Expand the main function to include benches for arith and vec_eq
+ctbench_main_with_seeds!((arith, Some(0x6b6c816d)), (vec_eq, None));
+// Alternatively, for no explicit seeds, you can use
+// ctbench_main!(arith, vec_eq);
